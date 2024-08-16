93f1c217155606c0e8344fa5550439d31b6aac53
Yhoo2 <yhooi2011@gmail.com>
2024-08-16 20:32:56 +0300

    dz4

--- Projejct1/lesson3.js
+++ Projejct1/lesson3.js
@@ -1,38 +1,12 @@
-// 1
-let minutes = 24;
-let quarter;
+let str = 'sdkhg';
+console.log(str.length);
 
-if (minutes <= 15) {
-	quarter = 'Первая';
-} else if (15 < minutes <= 30) {
-	quarter = 'Вторая';
-} else if (30 < minutes <= 45) {
-	quarter = 'Третья';
-} else {
-	quarter = 'Четвертая';
+str = "Alex";
+let newStr = '';
+let letter = '';
+
+for (i = 0; i < str.length; i++) {
+	letter = str[str.length - i - 1];
+	newStr = newStr + letter;
 }
-console.log(quarter + " четверть")
-
-// 2
-let numberMonth = 2;
-let season;
-
-if (numberMonth > 11 || numberMonth < 3) {
-	season = 'Зима';
-} else if (3 <= numberMonth < 6) {
-	season = 'Весна';
-} else if (6 <= numberMonth <  9) {
-	season = 'Лето';
-} else {
-	season = 'Осень';
-}
-console.log(season)
-
-// 3
-let num = 3;
-if (num % 2) {
-	num **= 3
-} else {
-	num **= 2
-}
-console.log(num)
+console.log(newStr);
