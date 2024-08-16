19b2013818780afc67d203652ebb9a6f87e7af4e
Yhoo2 <yhooi2011@gmail.com>
2024-08-16 15:54:16 +0300

    'dz3'

--- Projejct1/DZ2.js
+++ Projejct1/DZ2.js
@@ -1,4 +1,4 @@
-console.log(`В часе ${60*60}секунд`)
+console.log(`В часе ${60*60}секунд`);
 
 
 let a = 0;
@@ -8,5 +8,10 @@ a -= 8;
 a /= 4;
 a **= 3;
 a % 5;
-console.log(a)
-console.log('ананас' < "яблоко")
+console.log(a);
+console.log('ананас' < "яблоко");
+console.log('2' > '12')
+console.log(undefined == null)
+console.log(undefined === null)
+console.log(null == '0')
+console.log(null === +'0')
